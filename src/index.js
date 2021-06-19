3c4650b3a987eee3db1c4d2c5473c6e3883acfbe
George Grainger <GraingerGY@cardiff.ac.uk>
2021-06-19 01:23:36 +0100

    Increase size of game board

--- src/index.js
+++ src/index.js
@@ -142,15 +142,18 @@ class Game extends React.Component {
 
         return (
         <div className="game">
-            <div className="game-board">
-                <Board
-                    squares={ current.squares }
-                    onClick={ (i) => this.handleClick(i) }
-                />
-            </div>
-            <div className="game-info">
-                <div>{status}</div>
-                <ol>{moves}</ol>
+            <h1 className="title">Tick-Tac-Toe</h1>
+            <div className="game-container">
+                <div className="game-board">
+                    <Board
+                        squares={ current.squares }
+                        onClick={ (i) => this.handleClick(i) }
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
             </div>
         </div>
     );
